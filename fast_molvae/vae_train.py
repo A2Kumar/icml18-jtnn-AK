f103ee822b73236725d5adb60caedb731c8f0e88
Arun Kumar <arun.anthony.kumar@gmail.com>
2020-02-19 17:15:41 +0000

    changes

--- fast_molvae/vae_train.py
+++ fast_molvae/vae_train.py
@@ -24,7 +24,7 @@ parser.add_argument('--save_dir', required=True)
 parser.add_argument('--load_epoch', type=int, default=0)
 
 parser.add_argument('--hidden_size', type=int, default=450)
-parser.add_argument('--batch_size', type=int, default=32)
+parser.add_argument('--batch_size', type=int, default=16)
 parser.add_argument('--latent_size', type=int, default=56)
 parser.add_argument('--depthT', type=int, default=20)
 parser.add_argument('--depthG', type=int, default=3)

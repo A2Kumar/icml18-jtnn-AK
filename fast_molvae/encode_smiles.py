87cd32ef5bd4469cb77ef17d6193ecb7dc104423
Arun Kumar <arun.anthony.kumar@gmail.com>
2020-02-23 20:59:52 +0000

    num change

--- fast_molvae/encode_smiles.py
+++ fast_molvae/encode_smiles.py
@@ -20,7 +20,7 @@ vocab_path = './vocab.txt'
 
 vocab = [x.strip("\r\n ") for x in open(vocab_path)] 
 vocab = Vocab(vocab)
-prefix = '13'
+prefix = '14'
 model = JTNNVAE(vocab, 500, 128, 20, 3)
 model.load_state_dict(torch.load('vae_model/model.iter-5500-nokl'))
 model = model.cuda()
